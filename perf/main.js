9cb389a561d20d1fc15085b5c11ca9b1c8daff64
Jacopo Scazzosi <me@jacoscaz.com>
2018-06-26 00:51:20 +0100

    improves performance test with more metrics

--- perf/main.js
+++ perf/main.js
@@ -1,34 +1,79 @@
 
 'use strict';
 
+const Stats = require('stats-incremental');
 const LoopyLoop = require('../');
 
-let start;
+function round(num) {
+  return Math.round(num * 1e2) / 1e2;
+}
 
+function hrtimeToNanos(hrtime) {
+  return (hrtime[0] * 1e9) + hrtime[1];
+}
+
+function hrtimeToMillis(hrtime) {
+  return hrtimeToNanos(hrtime) / 1e6;
+}
+
+function hrtimeDiff(after, before) {
+  return [
+    after[0] - before[0], 
+    after[1] - before[1]
+  ];
+}
+
+function getStats(hrtimes) {
+  const stats = new Stats();
+  const length = hrtimes.length;
+  for (let i = length - 1; i >= 1; i--) {
+    stats.update(hrtimeToNanos(hrtimeDiff(hrtimes[i], hrtimes[i - 1])));
+  }
+  return stats.getAll();
+}
+
+function findMaxIndex(values) {
+  let max = -Infinity;
+  let pos = -Infinity;
+  for (let i = values.length - 1; i >= 0; i--) {
+    if (values[i] > max) {
+      max = values[i];
+      pos = i;
+    }
+  }
+  return [max, pos];
+}
+
+const limit = 1e6;
+const opts = {maxChained: 10};
+
+let start;
 let count = 0;
-let limit = 1e7;
+
+const hrtimes = new Array(limit);
 
 const task = (() => {
+  hrtimes[count] = process.hrtime();
   count++ >= limit && loop.stop();
   return Promise.resolve();
 });
 
-const opts = {maxChained: 10};
-
 const loop = new LoopyLoop(task, opts)
   .on('stopped', () => {
-    const delta = process.hrtime(start);
-    const millis = Math.round((delta[0] * 1e9 + delta[1]) / 1e6);
-    console.log(`${limit} cycles in ${millis} ms - ${limit/millis} cycles/ms`);
-  })
-  .on('started', () => {
-    start = process.hrtime();
-  })
-  .start();
+    const millis = hrtimeToMillis(process.hrtime(start));
+    console.log('');
+    console.log(`TOTAL: ${limit} ticks in ${round(millis / 1e3)} seconds`);
+    console.log(`SPEED: ${round(limit/millis)} tick/ms`);
+    console.log(`MAX CHAINED: ${opts.maxChained}`);
+    console.log('');
+    const stats = getStats(hrtimes);
+    console.log(`AVG: ${round(stats.mean)} ns/tick`);
+    console.log(`STD: ${round(stats.standard_deviation)} ns/tick`);
+    console.log(`COV: ${round(stats.standard_deviation / stats.mean)}`);
+    console.log(`MAX: ${stats.max} ns/tick`);
+    console.log(`MIN: ${stats.min} ns/tick`);
+    console.log('');
+  });
 
-
-
-
-
-
-
+start = process.hrtime();
+loop.start();
